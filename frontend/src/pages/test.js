f20c86992f46b513ebc0d8cacafb25807f23531e
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-21 03:22:34 +0530

    added test in dev1

---
+++ frontend/src/pages/test.js
@@ -0,0 +1,69 @@
+import { Button, Container, Text } from '@chakra-ui/react';
+import axios from 'axios';
+import { MD5 } from 'crypto-js';
+import { useState } from 'react';
+
+export default function Test() {
+  const [data, setData] = useState([]);
+
+  const generateDigestHash = (
+    username,
+    password,
+    realm,
+    method,
+    uri,
+    nonce,
+    nc,
+    cnonce,
+  ) => {
+    const ha1 = MD5(`${username}:${realm}:${password}`);
+    const ha2 = MD5(`${method}:${uri}`);
+    const response = MD5(`${ha1}:${nonce}:${nc}:${cnonce}:auth:${ha2}`);
+    return response.toString();
+  };
+
+  const username = 'admin';
+  const password = 'Admin@123';
+  const realm = 'REST API';
+  const uri = 'https://apps.teamworkcss.com/oakwooduat/api/index/companies';
+  const nonce = '63f5052db69f6';
+  const nc = '00000001';
+  const cnonce = Math.random().toString(36).substring(2);
+  const method = 'GET';
+
+  const config = {
+    headers: {
+      'x-api-key': '91bcec91-ddf0-402c-b287-a03d3563c320',
+      Authorization: `Digest username="${username}", realm="${realm}", nonce="${nonce}", uri="${uri}", qop=auth, nc=${nc}, cnonce="${cnonce}", response="${generateDigestHash(
+        username,
+        password,
+        realm,
+        method,
+        uri,
+        nonce,
+        nc,
+        cnonce,
+      )}"`,
+    },
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        'https://apps.teamworkcss.com/oakwooduat/api/index/companies',
+        config,
+      );
+      console.log(response.data.data);
+      setData(response.data.data);
+      //console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container mt={50} maxW="xl" centerContent>
+      <Button colorScheme={'orange'} onClick={fetchData}>Company Lists</Button>
+    </Container>
+  );
+}
