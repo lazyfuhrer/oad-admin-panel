ecc957effa82965dc5d047a27a88a34e613b31b0
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-23 08:04:22 +0530

    updated company info page

--- frontend/src/components/CompanyInfoCard.js
+++ frontend/src/components/CompanyInfoCard.js
@@ -1,22 +1,23 @@
 import {Card, HStack, IconButton, Stack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { AiFillEdit } from 'react-icons/ai'
 
-function CompanyInfoCard({ one, two, three }) {
+function CompanyInfoCard({ one, two }) {
   return (
     <>
       <Card maxW={'4xl'} borderRadius={'md'}>
         <Stack direction={'row'} align="center" p={3}>
-            <HStack w={'200px'} ml={10}>
+            <HStack w={'285px'} ml={10}>
                 <Text fontWeight={'bold'}>{one}</Text>
             </HStack>
-            <VStack w={'xl'} alignItems="flex-start" spacing={'-1'}>
+            <VStack w={'md'} alignItems="flex-start" spacing={'-1'}>
                 <Text>{two}</Text>
             </VStack>
-            <HStack >
+            <HStack>
                 <IconButton
                     variant={'ghost'}
                     aria-label="open menu"
-                    icon={three}
+                    icon={<AiFillEdit />}
                 />
             </HStack>
         </Stack>
--- frontend/src/pages/companyinfo.js
+++ frontend/src/pages/companyinfo.js
@@ -1,10 +1,22 @@
+import CompanyInfoCard from '@/components/CompanyInfoCard'
+import { Stack } from '@chakra-ui/react'
 import React from 'react'
-import ProfileCard from '../components/ProfileCard'
 
 function CompanyInfo() {
   return (
     <>
-      <ProfileCard name={'JILL HILL SMITH'}/>
+      <Stack spacing={'2'} mb={'1'}>
+        <CompanyInfoCard one={"UEN"} two={""}/>
+        <CompanyInfoCard one={"COMAPNY NAME"} two={"SUPERSTAR UNITED INC."} />
+        <CompanyInfoCard one={"INCORPORATION DATE"} two={"10/01/2022"} />
+        <CompanyInfoCard one={"COMPANY TYPE"} two={""} />
+        <CompanyInfoCard one={"PRINCIPAL ACTIVITY 1"} two={""} />
+        <CompanyInfoCard one={"PRINCIPAL ACTIVITY 2"} two={""} />
+        <CompanyInfoCard one={"REGISTERED OFFICE ADDRESS"} two={"7030 ANG MO KKO AVENUE 5 #08-94 NORTHSTAR @AMK SINGAPORE 569880"} />
+        <CompanyInfoCard one={"FINANCIAL YEAR END"} two={""} />
+        <CompanyInfoCard one={"DATE OF LAST AGM"} two={""} />
+        <CompanyInfoCard one={"WEBSITE"} two={""} />
+      </Stack>
     </>
   )
 }
