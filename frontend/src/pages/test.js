cd97e3e6ec77e3c31f08f028e57f7911758f54e1
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-30 09:46:55 +0530

    updated

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -63,15 +63,14 @@ export default function Test() {
     fetchData();
   }, []);
 
-  if (data.length > 0) {
-    console.log(data);
-  }
+  // if (data.length > 0) {
+  //   console.log(data[0].entity_name);
+  // }
 
   return (
     <Container maxW="xl">
-      {data.map((company) => (
-        <Text key={company.id}>{company.entity_name}</Text>
-      ))}
+      {data[0] && <Text>{data[0].entity_name}</Text>}
     </Container>
   );
+  
 }
