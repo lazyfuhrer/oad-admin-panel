a9748085fecd90fc229d39c49187957f3daf2122
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-31 11:29:58 +0530

    updated

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,11 +1,16 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import Login from './login';
 
 export default function App({ Component, pageProps }) {
+
+  const isLoggedIn = false;
+
   return (
     <ChakraProvider>
-      <Layout children={<Component {...pageProps} />}/>
+      {/* <Layout children={<Component {...pageProps} />}/> */}
+      {isLoggedIn ? <Layout children={<Component {...pageProps} />}/> : <Login/>}
     </ChakraProvider>
   )
 }
---
+++ frontend/src/pages/login.js
@@ -0,0 +1,62 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+  
+  export default function Login() {
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Log in to your account</Heading>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="username">
+                <FormLabel>Username</FormLabel>
+                <Input type="username"/>
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}>
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={'blue.400'}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Log in
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
