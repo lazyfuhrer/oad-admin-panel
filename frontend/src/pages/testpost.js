6c5662f0b753bdd486a68665bbcf3094225db6f9
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-04-15 10:45:00 +0530

    updated

--- frontend/src/pages/testpost.js
+++ frontend/src/pages/testpost.js
@@ -1,4 +1,4 @@
-import { Button, Container, Text } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import axios from 'axios';
 import { MD5 } from 'crypto-js';
 import { useState, useEffect } from 'react';
@@ -7,15 +7,7 @@ export default function Test() {
   const [data, setData] = useState([]);
 
   const generateDigestHash = (
-    username,
-    password,
-    realm,
-    method,
-    uri,
-    nonce,
-    nc,
-    cnonce,
-  ) => {
+    username, password, realm, method, uri, nonce, nc, cnonce, ) => {
     const ha1 = MD5(`${username}:${realm}:${password}`);
     const ha2 = MD5(`${method}:${uri}`);
     const response = MD5(`${ha1}:${nonce}:${nc}:${cnonce}:auth:${ha2}`);
@@ -25,50 +17,42 @@ export default function Test() {
   const username = 'admin';
   const password = 'Admin@123';
   const realm = 'REST API';
-  const uri = 'https://apps.teamworkcss.com/oakwooduat/api/index/companies';
+  const uri = 'https://apps.teamworkcss.com/oakwooduat/api/index/add_company';
   const nonce = '63f5052db69f6';
   const nc = '00000001';
   const cnonce = Math.random().toString(36).substring(2);
-  const method = 'GET';
+  const method = 'POST';
 
   const config = {
     headers: {
       'x-api-key': '91bcec91-ddf0-402c-b287-a03d3563c320',
       Authorization: `Digest username="${username}", realm="${realm}", nonce="${nonce}", uri="${uri}", qop=auth, nc=${nc}, cnonce="${cnonce}", response="${generateDigestHash(
-        username,
-        password,
-        realm,
-        method,
-        uri,
-        nonce,
-        nc,
-        cnonce,
-      )}"`,
+        username, password, realm, method, uri, nonce, nc, cnonce, )}"`,
     },
   };
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(
-        'https://apps.teamworkcss.com/oakwooduat/api/index/companies',
+      const payload = { companyid: '505', entity_name: 'NewAgeBankCorp', registration_no: '601', entity_type: '12', css_client: '1', country: 'Singapore' };
+      console.log(payload); // log the payload object
+      const response = await axios.post(
+        uri,
+        payload,
         config,
       );
-      console.log(response.data.data);
-      setData(response.data.data);
+      setData(response.data);
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    console.log(data)
+  }, [data]);
 
   return (
-    <Container maxW="xl">
-      {data.map((entity, index) => (
-        <Text key={index}>{entity.entity_name} {entity.country}</Text>
-      ))}
-    </Container>
+    <>
+        <Button colorScheme='green' onClick={fetchData}>Send data</Button>
+    </>
   );
 }
