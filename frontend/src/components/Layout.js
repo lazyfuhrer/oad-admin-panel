38b940fdda77781473a078ae2fc334d2baa8b6c2
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-04-18 18:28:32 +0530

    updated company list dropdown

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react"
+import React, { useContext, useEffect, useRef, useState } from "react"
 import {
   IconButton,
   Avatar,
@@ -19,22 +19,20 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
-  Heading,
-  Stack,
-  Button,
-  Center,
   InputGroup,
   InputRightElement,
   Input,
-  useFullscreen,
-  Divider
+  Divider,
+  Select
 } from "@chakra-ui/react"
 import { FiHome, FiTrendingUp, FiCompass, FiStar, FiSettings, FiMenu, FiBell, FiChevronDown, FiSearch, FiMaximize, FiAlignRight, FiEdit, FiActivity, FiLogOut } from "react-icons/fi"
 import { CgProfile} from "react-icons/cg"
-import { AiFillCaretDown} from "react-icons/ai"
 import NextLink from 'next/link'
 import { useRouter } from "next/router"
 import { UserContext } from "@/context/UserContext"
+import { MD5 } from "crypto-js"
+import axios from "axios"
+
 
 const LinkItems = [
   { name: "DASHBOARD", icon: FiHome, to: '/' },
@@ -82,6 +80,66 @@ export default function Layout({ children }) {
 }
 
 const SidebarContent = ({ onClose, ...rest }) => {
+  const [data, setData] = useState([]);
+
+  const generateDigestHash = (
+    username,
+    password,
+    realm,
+    method,
+    uri,
+    nonce,
+    nc,
+    cnonce,
+  ) => {
+    const ha1 = MD5(`${username}:${realm}:${password}`);
+    const ha2 = MD5(`${method}:${uri}`);
+    const response = MD5(`${ha1}:${nonce}:${nc}:${cnonce}:auth:${ha2}`);
+    return response.toString();
+  };
+
+  const username = 'admin';
+  const password = 'Admin@123';
+  const realm = 'REST API';
+  const uri = 'https://apps.teamworkcss.com/oakwooduat/api/index/companies';
+  const nonce = '63f5052db69f6';
+  const nc = '00000001';
+  const cnonce = Math.random().toString(36).substring(2);
+  const method = 'GET';
+
+  const config = {
+    headers: {
+      'x-api-key': '91bcec91-ddf0-402c-b287-a03d3563c320',
+      Authorization: `Digest username="${username}", realm="${realm}", nonce="${nonce}", uri="${uri}", qop=auth, nc=${nc}, cnonce="${cnonce}", response="${generateDigestHash(
+        username,
+        password,
+        realm,
+        method,
+        uri,
+        nonce,
+        nc,
+        cnonce,
+      )}"`,
+    },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://apps.teamworkcss.com/oakwooduat/api/index/companies',
+          config,
+        );
+        //console.log(response.data.data)
+        setData(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
   const { firstname, lastname, role } = useContext(UserContext);
   const router = useRouter();
   const isActive = (pathname) => router.pathname === pathname;
@@ -110,12 +168,13 @@ const SidebarContent = ({ onClose, ...rest }) => {
             fontSize={'14px'}
             fontWeight={'bold'}
           >
-            Superstar United Inc
-            <Icon
-              ml='3'
-              fontSize="16"
-              as={AiFillCaretDown}
-            />
+            <Select width="200px" defaultValue={data.length > 0 ? data[0].entity_name : ''}>
+              {data.map((company, index) => (
+                <option key={index} value={company.entity_name}>
+                  {company.entity_name}
+                </option>
+              ))}
+            </Select>
           </Flex>
         </VStack>
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
