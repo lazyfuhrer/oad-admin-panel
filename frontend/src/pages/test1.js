5d7709185800aa4f14a76eb8024386e1f4e0fe79
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-04-25 22:32:04 +0530

    updated

--- frontend/src/pages/test1.js
+++ frontend/src/pages/test1.js
@@ -1,21 +1,10 @@
-import { Button, Container, Text } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import axios from 'axios';
 import { MD5 } from 'crypto-js';
-import { useState } from 'react';
 
 export default function Test() {
-  const [data, setData] = useState([]);
 
-  const generateDigestHash = (
-    username,
-    password,
-    realm,
-    method,
-    uri,
-    nonce,
-    nc,
-    cnonce,
-  ) => {
+  const generateDigestHash = ( username, password, realm, method, uri, nonce, nc, cnonce, ) => {
     const ha1 = MD5(`${username}:${realm}:${password}`);
     const ha2 = MD5(`${method}:${uri}`);
     const response = MD5(`${ha1}:${nonce}:${nc}:${cnonce}:auth:${ha2}`);
@@ -32,49 +21,38 @@ export default function Test() {
   const method = 'POST';
 
   const addCompany = async () => {
-    const companyData = {
-      //css_client: '1',
-      //entity_name: 'XYZ Company',
-      Country: 'Singapore',
-      //default_address: 'Example Address',
-    };
-
+    const formData = new FormData();
+    formData.append('country', 'India');
+  
     try {
-        /*const nonce = '63f5052db69f6';
-        const nc = '00000001';
-        const cnonce = Math.random().toString(36).substring(2);
-        const method = 'POST';
-        const requestBody = JSON.stringify(companyData);*/
-        const response = await axios.post(
-          endpoint,
-          requestBody,
-          {
-            headers: {
-              'x-api-key': '91bcec91-ddf0-402c-b287-a03d3563c320',
-              Authorization: `Digest username="${username}", realm="${realm}", nonce="${nonce}", uri="${endpoint}", qop=auth, nc=${nc}, cnonce="${cnonce}", response="${generateDigestHash(
-                username,
-                password,
-                realm,
-                method,
-                endpoint,
-                nonce,
-                nc,
-                cnonce,
-              )}"`,
-              'Content-Type': 'application/json'
-            }
-          }
-        );
-        console.log('first');
-        console.log(response.data);
-        //setData(response.data);
-      } catch (error) {
-        console.log(error);
-      }
+      const response = await axios.post(
+        endpoint,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'x-api-key': '91bcec91-ddf0-402c-b287-a03d3563c320',
+            Authorization: `Digest username="${username}", realm="${realm}", nonce="${nonce}", uri="${endpoint}", qop=auth, nc=${nc}, cnonce="${cnonce}", response="${generateDigestHash(
+              username,
+              password,
+              realm,
+              method,
+              endpoint,
+              nonce,
+              nc,
+              cnonce,
+            )}"`,
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }  
-      return (
-        <>
-          <Button colorScheme={'blue'} onClick={addCompany}>Add Data</Button>
-        </>
-      )
-}      
+  return (
+    <>
+      <Button colorScheme={'blue'} onClick={addCompany}>Add Data</Button>
+    </>
+  );
+};
