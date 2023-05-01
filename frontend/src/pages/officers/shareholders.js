a17c93331cd04eebf66bf24926916d1d3a089a9c
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-01 19:07:02 +0530

    shareholders added

--- frontend/src/pages/officers/shareholders.js
+++ frontend/src/pages/officers/shareholders.js
@@ -1,6 +1,14 @@
+import ProfileCard from "@/components/ProfileCard";
+import { UserContext } from "@/context/UserContext";
+import { useContext } from "react";
 
 export default function Shareholders() {
+  const { allCompanies } = useContext(UserContext);
   return (
-    <div>shareholders</div>
-  )
+    <>
+      {allCompanies && allCompanies[2].shareholders.map((shareholder, index) => (
+        <ProfileCard key={index} name={shareholder.data.nameOfShareholder}/>
+      ))}
+    </>
+  );
 };
