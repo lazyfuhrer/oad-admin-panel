7c613df74356ac9039a1056f3ee16d730f086ebb
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-05-01 15:22:40 +0530

    updated

--- frontend/src/pages/officers/directors.js
+++ frontend/src/pages/officers/directors.js
@@ -1,6 +1,17 @@
+import CompanyInfoCard from '@/components/CompanyInfoCard'
+import CompanyInfoCardSmall from '@/components/CompanyInfoCardSmall';
+import OfficerProfile from '@/components/OfficerProfile';
+import OfficerProfileCard from '@/components/OfficerProfileCard';
+import ProfileCard from '@/components/ProfileCard';
+import { Box, Flex, HStack, Stack } from '@chakra-ui/react';
 
 export default function Directors() {
+  const officers = ['JILL HILL SMITH', 'JACK SUPERSTAR DENVERS', 'HADWICK BOTZMAN'];
   return (
-    <div>directors</div>
+    <>
+      {officers.map((officer, index) => (
+        <ProfileCard key={index} name={officer}/>
+      ))}
+    </>
   )
-};
+}
